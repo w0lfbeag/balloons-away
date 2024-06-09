e77950fba9f6f130498233a16b76335e79af0f1d
w0lfbeag <cuanmcdonnell27@gmail.com>
2024-06-09 11:09:46 +0100

    Initial commit

---
+++ src/main/java/wolfbeag/balloonsaway/BalloonsAwayDataGenerator.java
@@ -0,0 +1,11 @@
+package wolfbeag.balloonsaway;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BalloonsAwayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
